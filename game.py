322f86777946a8a93c86f6c92916c89bed3642ee
matkw <mateusz.j.kwiatkowski@gmail.com>
2021-04-20 20:55:16 +0200

    Adding updated code

--- game.py
+++ game.py
@@ -9,40 +9,36 @@ class Game:
     """
 
     def __init__(self):
-        pass
+        self._step = 15
+        self.snake = Snake(20, 30)
+        self.run = True
+        self.movement_flag = 0
+        self.game_window = Window()
+
+    def check_if_key_pressed(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and self.movement_flag != 2:
+            self.movement_flag = 1
+        if keys[pygame.K_RIGHT] and self.movement_flag != 1:
+            self.movement_flag = 2
+        if keys[pygame.K_UP] and self.movement_flag != 4:
+            self.movement_flag = 3
+        if keys[pygame.K_DOWN] and self.movement_flag != 3:
+            self.movement_flag = 4
 
     def do_game(self):
-        game_window = Window()
-        run = True
-        snake = Snake(20, 30)
-        step = 15
-        movement_flag = 0
-
-        while run:
+        while self.run:
             pygame.time.delay(120)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    run = False
-            keys = pygame.key.get_pressed()
-
-            if keys[pygame.K_LEFT]:
-                movement_flag = 1
-            if keys[pygame.K_RIGHT]:
-                movement_flag = 2
-            if keys[pygame.K_UP]:
-                movement_flag = 3
-            if keys[pygame.K_DOWN]:
-                movement_flag = 4
-
-            if movement_flag == 1:
-                snake.x -= step
-            if movement_flag == 2:
-                snake.x += step
-            if movement_flag == 3:
-                snake.y -= step
-            if movement_flag == 4:
-                snake.y += step
-
-            game_window.refill_game_window()
-            snake.draw_snake_element(game_window.get_window())
+                    self.run = False
+            # Key pressed
+            self.check_if_key_pressed()
+            # Move snake
+            self.snake.move_snake(self.movement_flag, self._step)
+            # Clear game window
+            self.game_window.refill_game_window()
+            # Draw snake
+            self.snake.draw_snake_element(self.game_window.get_window())
+            # Refresh game window
             pygame.display.update()
--- snake.py
+++ snake.py
@@ -6,13 +6,22 @@ class Snake:
     This is class with snake body parameters
     """
 
-    def __init__(self, x = 20, y = 20):
+    def __init__(self, x=20, y=20):
         self._width = 15
         self._height = 15
         self._color = (255, 0, 0)
         self.x = x
         self.y = y
 
-
     def draw_snake_element(self, window):
         pygame.draw.rect(window, self._color, (self.x, self.y, self._width, self._height))
+
+    def move_snake(self, movement_flag, step):
+        if movement_flag == 1:
+            self.x -= step
+        if movement_flag == 2:
+            self.x += step
+        if movement_flag == 3:
+            self.y -= step
+        if movement_flag == 4:
+            self.y += step
